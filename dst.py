755471d1dbc8a14d78150247a8e8d645781ee7e9
Vikas Nagare <47071298+VikasNagare45@users.noreply.github.com>
2020-07-27 12:15:12 +0530

    --- Auto-Git Commit ---

--- dst.py
+++ dst.py
@@ -1,8 +1,15 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 import selenium as sel
 import pandas as pd
 import matplotlib as mlt
-
 print("Program started")
-
 # load dataset
-dataset = pd.read_csv("./")
+dataset = pd.read_csv("/Users/vikasnagare/Desktop/final_dataset.csv")
+a = dataset.iloc[1:1]
+
+browser = webdriver.Chrome()
+browser.get("/Users/vikasnagare/Desktop/cromed/chromedriver")
+
+driver.navigate().to("http://www.instagram.com/")
